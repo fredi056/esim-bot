5d9a2d016b41b642fb8e281e987f2e44aba73a46
fredi056 <fredi056@ya.ru>
2026-04-13 13:05:17 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,8 @@ from typing import Optional, Dict, List, Tuple
 import telebot
 from telebot import types
 
-TOKEN = os.getenv("TOKEN")
-ADMIN_ID_RAW = os.getenv("ADMIN_ID")
-
+TOKEN = [REDACTED_TELEGRAM_BOT_TOKEN]
+ADMIN_ID_RAW = 1157537072
 if not TOKEN:
     raise ValueError("TOKEN not found")
 if not ADMIN_ID_RAW:
